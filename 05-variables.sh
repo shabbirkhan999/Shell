5ff8ef7a929611aff9b2418fbd7f5870f7b44d97
shabbir khan <khan.shabbir999@gmail.com>
2023-08-20 18:58:14 +0530

    Variables  &

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-echo "Pls enter password"
+echo "Pls enter username"
 
-read USERNAME #this is variable where the value of one user entered
+read -s USERNAME #this is variable where the value of one user entered
 
 echo "Username entered is : $USERNAME"
+
+
+echo "Pls enter Password"
+
+read -s PASSWORD
+
+echo "Password entered is : $PASSWORD"
